e8ac26e55ab2ed3ece662fd5977ff650b0799636
naufal <naufalopo87@gmail.com>
2025-11-10 12:43:08 +0700

    feat: add gaps and insets

---
+++ lib/src/core/constants/gaps.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'insets.dart';
+
+class Gaps {
+  static const h8 = SizedBox(width: Insets.x2);
+  static const h12 = SizedBox(width: Insets.x3);
+  static const h16 = SizedBox(width: Insets.x4);
+
+  static const v8 = SizedBox(height: Insets.x2);
+  static const v12 = SizedBox(height: Insets.x3);
+  static const v16 = SizedBox(height: Insets.x4);
+  static const v20 = SizedBox(height: Insets.x5);
+  static const v24 = SizedBox(height: Insets.x6);
+  static const v32 = SizedBox(height: Insets.x8);
+  static const v40 = SizedBox(height: Insets.x10);
+  static const v48 = SizedBox(height: Insets.x12);
+}
